e580c03a3a3ab7ee8da0c0d2213cb81a36f05f7f
Jayavel <jayavel1520@gmail.com>
2024-11-06 22:42:10 +0530

    Creating Strings two ways

---
+++ StringInternal/Strings.java
@@ -0,0 +1,11 @@
+public class Strings {
+    public static void main(String[] args) {
+        // String creation using litetrals
+        String myName = "Jayavel";
+        System.out.println(myName);
+
+        // String creation using new keyword
+        String myName1 = new String("Jayavel");
+        System.out.println(myName1); 
+    }
+}
