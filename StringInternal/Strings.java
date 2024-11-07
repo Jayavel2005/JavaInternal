fdad1c463fa34e815642197413e43304a1bbbe7d
Jayavel <jayavel1520@gmail.com>
2024-11-07 22:32:56 +0530

    Basics of Strings

--- StringInternal/Strings.java
+++ StringInternal/Strings.java
@@ -1,11 +1,34 @@
+import java.util.Scanner;
+
 public class Strings {
     public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
         // String creation using litetrals
         String myName = "Jayavel";
         System.out.println(myName);
 
         // String creation using new keyword
         String myName1 = new String("Jayavel");
-        System.out.println(myName1); 
+        System.out.println(myName1);
+
+        System.out.print("Enter the Word: ");
+        String word = scanner.nextLine();
+        // Finding length
+        System.out.println(word.length());
+        // Finding Any Char using index
+        System.out.println(word.charAt(0));
+        
+        System.out.println(word.toUpperCase());
+        System.out.println(word.toLowerCase());
+
+        // Compare Two Strings
+
+        String word1 = new String("Jaavel");
+        String word2 = new String("Jaavel");
+        System.out.println(word1.equalsIgnoreCase(word2));
+
+
     }
 }
---
+++ StringInternal/tempCodeRunnerFile.java
@@ -0,0 +1,1 @@
+reCase(word2));
